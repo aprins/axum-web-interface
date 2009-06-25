bd698965c504e80b18a2a9ae12f5c805d30e4c06
Yoran Heling <projects@yorhel.nl>
2009-06-25 07:23:53 +0000

    Added unique ID on /mambanet and tiny code cleanup
    The @mbn_types array wasn't used, so I removed it. Used JOIN to get parent
    name, instead of subquery (although the parent name isn't used at the
    moment, either...)

--- lib/AXUM/Handler/MambaNet.pm
+++ lib/AXUM/Handler/MambaNet.pm
@@ -11,9 +11,6 @@ YAWF::register(
 );
 
 
-my @mbn_types = ('no data', 'unsigned int', 'signed int', 'state', 'octet string', 'float', 'bit string');
-
-
 sub list {
   my $self = shift;
 
@@ -24,17 +21,19 @@ sub list {
     return $self->resRedirect('/mambanet');
   }
 
-  my $cards = $self->dbAll('SELECT a.addr, a.name, a.active, a.engine_addr, a.parent,
-    (SELECT name FROM addresses b WHERE (b.id).man = (a.parent).man AND (b.id).prod = (a.parent).prod AND (b.id).id = (a.parent).id) AS parent_name,
+  my $cards = $self->dbAll('SELECT a.addr, a.id, a.name, a.active, a.engine_addr, a.parent, b.name AS parent_name,
     (SELECT COUNT(*) FROM node_config n WHERE a.addr = n.addr) AS config_cnt,
-    (SELeCT COUNT(*) FROM defaults d WHERE a.addr = d.addr) AS default_cnt
+    (SELECT COUNT(*) FROM defaults d WHERE a.addr = d.addr) AS default_cnt
     FROM addresses a
+    JOIN addresses b ON (b.id).man = (a.parent).man AND (b.id).prod = (a.parent).prod AND (b.id).id = (a.parent).id
     ORDER BY a.addr');
+
   $self->htmlHeader(title => 'MambaNet configuration', page => 'mambanet');
   table;
-   Tr; th colspan => 7, 'MambaNet configuration'; end;
+   Tr; th colspan => 8, 'MambaNet configuration'; end;
    Tr;
     th 'Address';
+    th 'Unique ID';
     th 'Node name';
     th 'Engine';
     th 'Parent';
@@ -43,11 +42,13 @@ sub list {
     th '';
    end;
    for my $c (@$cards) {
+     $c->{$_} =~ s/\((\d+),(\d+),(\d+)\)/sprintf($1?'%04X:%04X:%04X':'-', $1, $2, $3)/e
+       for ('parent', 'id');
      Tr !$c->{active} ? (class => 'inactive') : ();
       td sprintf '%08X', $c->{addr};
+      td $c->{id};
       td $c->{name};
       td sprintf '%08X', $c->{engine_addr};
-      $c->{parent} =~ s/\((\d+),(\d+),(\d+)\)/sprintf($1?'%04X:%04X:%04X':'-', $1, $2, $3)/e;
       td $c->{parent};
       td !$c->{default_cnt} ? (class => 'inactive') : (), $c->{default_cnt};
       td !$c->{config_cnt} ? (class => 'inactive') : (), $c->{config_cnt};
@@ -63,6 +64,7 @@ sub list {
   end;
   $self->htmlFooter;
 }
+
 
 1;
 
