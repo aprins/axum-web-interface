1977ee30a56be9b64ef763cb22cd1419423b2052
Anton Prins <a.prins@d-r.nl>
2009-07-27 14:23:32 +0200

    Module routing presets can be configured now.

--- lib/AXUM/Handler/ModuleRouting.pm
+++ lib/AXUM/Handler/ModuleRouting.pm
@@ -9,7 +9,7 @@ use Data::Dumper 'Dumper';
 
 YAWF::register(
   qr{module/([1-9][0-9]*)/route} => \&route,
-  qr{ajax/module/route} => \&ajax,
+  qr{ajax/module/route/([1-8])} => \&ajax,
 );
 
 
@@ -18,23 +18,23 @@ my @balance = ('left', 'center', 'right');
 
 
 sub _col {
-  my($n, $d) = @_;
+  my($n, $p, $d) = @_;
   my $v = $d->{$n};
   if($n =~ /level$/) {
-    a href => '#', onclick => sprintf('return conf_level("module/route", %d, "%s", %f, this)', $d->{number}, $n, $v),
+    a href => '#', onclick => sprintf('return conf_level("module/route/%d", %d, "%s", %f, this)', $p, $d->{number}, $n, $v),
       $v == 0 ? (class => 'off') : (), sprintf '%.1f dB', $v;
   }
   if($n =~ /on_off$/) {
-    a href => '#', onclick => sprintf('return conf_set("module/route", %d, "%s", "%s", this)', $d->{number}, $n, $v?0:1),
+    a href => '#', onclick => sprintf('return conf_set("module/route/%d", %d, "%s", "%s", this)', $p, $d->{number}, $n, $v?0:1),
       !$v ? (class => 'off', 'off') : 'on';
   }
   if($n =~ /pre_post$/) {
-    a href => '#', onclick => sprintf('return conf_set("module/route", %d, "%s", "%s", this)', $d->{number}, $n, $v?0:1),
+    a href => '#', onclick => sprintf('return conf_set("module/route/%d", %d, "%s", "%s", this)', $p, $d->{number}, $n, $v?0:1),
       !$v ? (class => 'off', 'post') : 'pre';
   }
   if($n =~ /balance$/) {
     $v = sprintf '%.0f', $v/512;
-    a href => '#', onclick => sprintf('return conf_select("module/route", %d, "%s", %d, this, "balance_items")',
+    a href => '#', onclick => sprintf('return conf_select("module/route/%d", %d, "%s", %d, this, "balance_items")', $p,
       $d->{number}, $n, $v), $v == 1 ? (class => 'off') : (), $balance[$v];
   }
 }
@@ -43,9 +43,13 @@ sub _col {
 sub route {
   my($self, $nr) = @_;
 
+  my $p = $self->formValidate({name => 'p', required => 0, default => 1, enum => [1..8]});
+  return 404 if $p->{_err};
+  $p = $p->{p};
+
   my $bus = $self->dbAll('SELECT number, label FROM buss_config ORDER BY number');
   my $mod = $self->dbRow('SELECT number, !s FROM module_config WHERE number = ?',
-    join(', ', map +("${_}_level[1]", "${_}_on_off[1]", "${_}_pre_post[1]", "${_}_balance[1]", "${_}_assignment"), @busses), $nr);
+    join(', ', map +("${_}_level[$p]", "${_}_on_off[$p]", "${_}_pre_post[$p]", "${_}_balance[$p]", "${_}_assignment"), @busses), $nr);
   return 404 if !$mod->{number};
 
   $self->htmlHeader(page => 'modulerouting', section => $nr, title => "Module $nr routing configuration");
@@ -55,7 +59,17 @@ sub route {
    end;
   end;
   table;
-   Tr; th colspan => 5, "Module $nr routing configuration"; end;
+   Tr;
+    th colspan => 5;
+     txt "Module $nr routing configuration";
+     p class => 'navigate';
+      txt 'preset: ';
+      a href => "?p=1", $p == 1 ? (class => 'sel') : (), 'Default';
+      a href => "?p=$_", $p == $_ ? (class => 'sel') : (), $_
+        for (2..8);
+     end;
+    end;
+   end;
    Tr;
     th 'Buss';
     th 'Level';
@@ -67,10 +81,10 @@ sub route {
      next if !$mod->{$busses[$b->{number}-1].'_assignment'};
      Tr;
       th $b->{label};
-      td; _col $busses[$b->{number}-1].'_level', $mod; end;
-      td; _col $busses[$b->{number}-1].'_on_off', $mod; end;
-      td; _col $busses[$b->{number}-1].'_pre_post', $mod; end;
-      td; _col $busses[$b->{number}-1].'_balance', $mod; end;
+      td; _col $busses[$b->{number}-1].'_level', $p, $mod; end;
+      td; _col $busses[$b->{number}-1].'_on_off', $p, $mod; end;
+      td; _col $busses[$b->{number}-1].'_pre_post', $p, $mod; end;
+      td; _col $busses[$b->{number}-1].'_balance', $p, $mod; end;
      end;
    }
   end;
@@ -79,7 +93,8 @@ sub route {
 
 
 sub ajax {
-  my $self = shift;
+  my($self, $p) = @_;
+
   my $f = $self->formValidate(
     { name => 'field', template => 'asciiprint' },
     { name => 'item', template => 'int' },
@@ -95,11 +110,11 @@ sub ajax {
   my %set;
   defined $f->{$_} && ($f->{$_} *= 511)
     for (map "${_}_balance", @busses);
-  defined $f->{$_} && ($set{$_."[1] = ?" } = $f->{$_})
+  defined $f->{$_} && ($set{$_."[$p] = ?" } = $f->{$_})
     for (map +("${_}_level", "${_}_on_off", "${_}_pre_post", "${_}_balance"), @busses);
 
   $self->dbExec('UPDATE module_config !H WHERE number = ?', \%set, $f->{item}) if keys %set;
-  _col $f->{field}, { number => $f->{item}, $f->{field} => $f->{$f->{field}} };
+  _col $f->{field}, $p, { number => $f->{item}, $f->{field} => $f->{$f->{field}} };
 }
 
 
